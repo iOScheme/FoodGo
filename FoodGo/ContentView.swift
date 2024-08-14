f3f766535b702f0fc56a63b70c29b47774a1de8b
AlexApostol <apostol516@gmail.com>
2024-08-14 16:30:04 +0200

    Initial Commit

---
+++ FoodGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodGo
+//
+//  Created by Alex.personal on 14/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodGo/FoodGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodGoApp.swift
+//  FoodGo
+//
+//  Created by Alex.personal on 14/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
