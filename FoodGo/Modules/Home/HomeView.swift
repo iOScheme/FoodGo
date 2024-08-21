cdda325f07cb2376af2952fc67131936b96b13f4
AlexApostol <apostol516@gmail.com>
2024-08-21 16:34:53 +0200

    scroll to current day

--- FoodGo/Modules/Home/HomeView.swift
+++ FoodGo/Modules/Home/HomeView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 
 struct HomeView: View {
     private let viewModel = HomeViewModel()
+    let days = HomeViewModel().getDaysInMonth()
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(viewModel.getDaysInMonth()) { dayData in
+        ScrollViewReader { proxy in
+            ScrollView {
+                ForEach(viewModel.getDaysInMonth(), id: \.self) { dayData in
                     VStack {
                         if dayData.currentDay {
                             Text("\(dayData.dayNumber)").foregroundStyle(.red)
-                            Text(dayData.dayName)
+                            Text(dayData.dayName).onAppear {
+                                proxy.scrollTo(dayData, anchor: .center)
+                            }
+                            
+                            
                         } else {
+                            Text(dayData.monthName)
                             Text("\(dayData.dayNumber)")
                             Text(dayData.dayName)
                         }
-                        
-                       
                     }
                 }
             }
--- FoodGo/Modules/Home/HomeViewModel.swift
+++ FoodGo/Modules/Home/HomeViewModel.swift
@@ -27,40 +27,53 @@ class HomeViewModel: ObservableObject {
         
         return dateFormatter
     }()
+    
+    private lazy var days: [DayData] = {
+       return getDaysInMonth()
+    }()
+    
+    lazy var currentDay: Int = {
+        calendar.component(.day, from: currentDate)
+    }()
+    
+    lazy var currentMonth: Int = {
+        calendar.component(.month, from: currentDate)
+    }()
+    
    
-    func getDaysInMonth() -> [DayData] {
+     func getDaysInMonth() -> [DayData] {
+        var components = DateComponents()
+        components.year = calendar.component(.year, from: Date())
+        components.month = 1
+        components.day = 1
         
-
-        let year = calendar.component(.year, from: currentDate)
-        let month = calendar.component(.month, from: currentDate)
-        let day = calendar.component(.day, from: currentDate)
-        
-        let dateComponents = DateComponents(year: year, month: month, day: day)
+        let dateComponents = DateComponents(year: components.year, month: components.month, day: components.day)
         
         // Get the current day of the month
         guard let startDate = calendar.date(from: dateComponents) else { return [] }
         
         // Determine the range of days in the month
-        guard let range = calendar.range(of: .day, in: .month, for: startDate) else { return [] }
+        guard let range = calendar.range(of: .day, in: .year, for: startDate) else { return [] }
         
         // Get the current day
-        let currentDay = calendar.component(.day, from: startDate)
-        
-        // Calculate the remaining days
-        let remainingDays = range.count - currentDay
         
         
         // Generate all dates within the range
-        return range.suffix(from: remainingDays).compactMap { day -> Date? in
-            let components = DateComponents(year: year, month: month, day: day)
+        return range.compactMap { day -> Date? in
+            let components = DateComponents(year: components.year, month: components.month, day: day)
             return calendar.date(from: components)
         }.map({
             DayData(
                 dayName: getDayName(from: $0),
                 dayNumber: getDayNumber(from: $0),
-                currentDay: getDayNumber(from: $0) == currentDay
+                currentDay: isCurrentDay(comparedDate: $0),
+                monthName: getMonthName(from: $0)
             )
         })
+    }
+    
+    private func isCurrentDay(comparedDate: Date) -> Bool {
+        return calendar.component(.day, from: comparedDate) == currentDay && calendar.component(.month, from: comparedDate) == currentMonth
     }
     
     func getDayName(from date: Date) -> String {
@@ -72,12 +85,19 @@ class HomeViewModel: ObservableObject {
     func getDayNumber(from date: Date) -> Int {
         return calendar.component(.day, from: date)
     }
+    
+    func getMonthName(from date: Date) -> String {
+        
+        dateFormatter.dateFormat = "MMMM" // "MMMM" gives full month name; use "MMM" for short name
+        return dateFormatter.string(from: date)
+    }
 }
 
-struct DayData: Identifiable {
+struct DayData: Identifiable, Hashable {
     let id = UUID()
     let dayName: String
     let dayNumber: Int
     let currentDay: Bool
+    let monthName: String
 }
 
